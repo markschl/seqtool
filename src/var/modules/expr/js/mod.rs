d0821c5e4c0794e03f64084a4b6ee74af318a7f3
markschl <markschl19@gmail.com>
2024-03-17 20:53:58 +0100

    Fix JS expression error reporting
    Return error instead of unwrapping

--- src/var/modules/expr/js/mod.rs
+++ src/var/modules/expr/js/mod.rs
@@ -19,29 +19,27 @@ pub fn replace_register_vars(script: &str, b: &mut VarBuilder) -> CliResult<(Str
     let mut vars = FxHashMap::default();
     match parse_script(script) {
         Ok(ast) => {
-            let new_code = ast
-                .rewrite(|name, args| {
-                    if b.has_var(name) {
-                        let func = try_opt!(st_func_from_parsed(name, args, false));
-                        b.register_nested_var(&func).transpose().map(|res| {
-                            res.map(|(var_id, _, _)| {
-                                // get unique placeholder variable name (function arguments are hashed)
-                                let js_varname = if func.num_args() == 0 {
-                                    func.name.clone()
-                                } else {
-                                    let mut hasher = FxHasher64::default();
-                                    func.args.hash(&mut hasher);
-                                    format!("{}_{:x}", func.name, hasher.finish())
-                                };
-                                vars.insert(var_id, js_varname.clone());
-                                js_varname
-                            })
+            let new_code = ast.rewrite(|name, args| {
+                if b.has_var(name) {
+                    let func = try_opt!(st_func_from_parsed(name, args, false));
+                    b.register_nested_var(&func).transpose().map(|res| {
+                        res.map(|(var_id, _, _)| {
+                            // get unique placeholder variable name (function arguments are hashed)
+                            let js_varname = if func.num_args() == 0 {
+                                func.name.clone()
+                            } else {
+                                let mut hasher = FxHasher64::default();
+                                func.args.hash(&mut hasher);
+                                format!("{}_{:x}", func.name, hasher.finish())
+                            };
+                            vars.insert(var_id, js_varname.clone());
+                            js_varname
                         })
-                    } else {
-                        None
-                    }
-                })
-                .unwrap();
+                    })
+                } else {
+                    None
+                }
+            })?;
             // dbg!(ast, &new_code);
             Ok((
                 new_code,
